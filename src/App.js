a2ec212db1b7c15c2fd520db18fccabc2fa8bd84
Andrew Gilev <147297725+i-andrey-i@users.noreply.github.com>
2024-10-17 21:28:58 +0500

    Autorization + css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import logo from './logo_urfu.png'
 
 function App() {
-  return (
-    <div className="App">
+  // состояния на заполнение input полей
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('') 
 
-    </div>
-  );
+  // Состояние на правильную запись email
+  const [emailError, setEmailError] = useState('Поле email не может быть пустым')
+
+  // Состояние на проверку были ли в input поле или нет
+  const [emailDirty, setEmailDirty] = useState(false)
+
+  // Состояние на блокирование формы
+  const [formValid, setFormValid] = useState(false)
+
+  const emailHandler = (e) =>{
+    setEmail(e.target.value)
+		const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		if (!re.test(String(e.target.value).toLowerCase())){
+      setEmailError('Email некорректный')
+    } else{
+      setEmailError('')
+    }
+  }
+
+  const blurHandler = (e) =>{
+    if(e.target.name === 'email'){
+      setEmailDirty(true)
+    }
+  }
+
+  useEffect( () =>{
+    if(emailError){
+      setFormValid(false)
+    } else{
+      setFormValid(true)
+    }
+  }, [emailError])
+
+  return (
+		<div className='App'>
+			<form className='form'>
+				<img src={logo} alt='' />
+				<h1>Авторизация</h1>
+
+				{emailError && emailDirty && (
+					<div className='warning'>
+						<b>{emailError}</b>
+					</div>
+				)}
+				<input
+					value={email}
+					onChange={e => emailHandler(e)}
+					onBlur={e => blurHandler(e)}
+					name='email'
+					type='text'
+					placeholder='Логин'
+				/>
+
+				<input
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					name='password'
+					type='password'
+					placeholder='Пароль'
+				/>
+				<button disabled={!formValid} type='submit'>
+					Войти
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default App;
